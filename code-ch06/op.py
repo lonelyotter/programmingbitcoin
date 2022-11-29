96ddfe77d4814ae1f3a6a3d2e1901b87d196b708
lonelyotter <57280827+lonelyotter@users.noreply.github.com>
2022-11-29 22:21:14 +0800

    finish ch06

--- code-ch06/op.py
+++ code-ch06/op.py
@@ -55,6 +55,8 @@ def decode_num(element):
 def op_0(stack):
     stack.append(encode_num(0))
     return True
+
+
 # end::source3[]
 
 
@@ -315,6 +317,8 @@ def op_dup(stack):
         return False
     stack.append(stack[-1])  # <2>
     return True
+
+
 # end::source1[]
 
 
@@ -647,7 +651,11 @@ def op_hash160(stack):
     # check that there's at least 1 element on the stack
     # pop off the top element from the stack
     # push a hash160 of the popped off element to the stack
-    raise NotImplementedError
+    if len(stack) < 1:
+        return False
+    element = stack.pop()
+    stack.append(hash160(element))
+    return True
 
 
 # tag::source2[]
@@ -657,6 +665,8 @@ def op_hash256(stack):
     element = stack.pop()
     stack.append(hash256(element))
     return True
+
+
 # end::source2[]
 
 
@@ -668,7 +678,21 @@ def op_checksig(stack, z):
     # parse the serialized pubkey and signature into objects
     # verify the signature using S256Point.verify()
     # push an encoded 1 or 0 depending on whether the signature verified
-    raise NotImplementedError
+    if len(stack) < 2:
+        return False
+    sec = stack.pop()
+    der = stack.pop()
+    der = der[:-1]
+    try:
+        point = S256Point.parse(sec)
+        sig = Signature.parse(der)
+    except (ValueError, SyntaxError):
+        return False
+    if point.verify(z, sig):
+        stack.append(encode_num(1))
+    else:
+        stack.append(encode_num(0))
+    return True
 
 
 def op_checksigverify(stack, z):
@@ -723,14 +747,17 @@ class OpTest(TestCase):
     def test_op_hash160(self):
         stack = [b'hello world']
         self.assertTrue(op_hash160(stack))
-        self.assertEqual(
-            stack[0].hex(),
-            'd7d5ee7824ff93f94c3055af9382c86c68b5ca92')
+        self.assertEqual(stack[0].hex(),
+                         'd7d5ee7824ff93f94c3055af9382c86c68b5ca92')
 
     def test_op_checksig(self):
         z = 0x7c076ff316692a3d7eb3c3bb0f8b1488cf72e1afcd929e29307032997a838a3d
-        sec = bytes.fromhex('04887387e452b8eacc4acfde10d9aaf7f6d9a0f975aabb10d006e4da568744d06c61de6d95231cd89026e286df3b6ae4a894a3378e393e93a0f45b666329a0ae34')
-        sig = bytes.fromhex('3045022000eff69ef2b1bd93a66ed5219add4fb51e11a840f404876325a1e8ffe0529a2c022100c7207fee197d27c618aea621406f6bf5ef6fca38681d82b2f06fddbdce6feab601')
+        sec = bytes.fromhex(
+            '04887387e452b8eacc4acfde10d9aaf7f6d9a0f975aabb10d006e4da568744d06c61de6d95231cd89026e286df3b6ae4a894a3378e393e93a0f45b666329a0ae34'
+        )
+        sig = bytes.fromhex(
+            '3045022000eff69ef2b1bd93a66ed5219add4fb51e11a840f404876325a1e8ffe0529a2c022100c7207fee197d27c618aea621406f6bf5ef6fca38681d82b2f06fddbdce6feab601'
+        )
         stack = [sig, sec]
         self.assertTrue(op_checksig(stack, z))
         self.assertEqual(decode_num(stack[0]), 1)
